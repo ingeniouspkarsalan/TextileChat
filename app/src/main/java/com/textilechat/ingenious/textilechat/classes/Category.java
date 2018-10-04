f8e04e73858bf6b3b360baa718919d8fd79142e4
DkKhan786 <ingenious.dilawar@gmail.com>
2018-10-04 17:33:56 +0500

    Home Page

---
+++ app/src/main/java/com/textilechat/ingenious/textilechat/classes/Category.java
@@ -0,0 +1,30 @@
+package com.textilechat.ingenious.textilechat.classes;
+
+public class Category
+{
+    String c_id, c_name, c_image;
+
+    public String getC_id() {
+        return c_id;
+    }
+
+    public void setC_id(String c_id) {
+        this.c_id = c_id;
+    }
+
+    public String getC_name() {
+        return c_name;
+    }
+
+    public void setC_name(String c_name) {
+        this.c_name = c_name;
+    }
+
+    public String getC_image() {
+        return c_image;
+    }
+
+    public void setC_image(String c_image) {
+        this.c_image = c_image;
+    }
+}

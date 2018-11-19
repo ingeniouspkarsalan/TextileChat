55350f4c801c4064c49172c4f38ecfa7ad31d1fd
ingenioupkarsalan <ingeniou.aarsalan@gmail.com>
2018-11-19 17:02:18 +0500

    changes

--- app/src/main/java/com/textilechat/ingenious/textilechat/classes/Main_class.java
+++ app/src/main/java/com/textilechat/ingenious/textilechat/classes/Main_class.java
@@ -3,6 +3,7 @@ package com.textilechat.ingenious.textilechat.classes;
 import android.app.Application;
 import android.content.ContextWrapper;
 
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.pixplicity.easyprefs.library.Prefs;
 
 public class Main_class extends Application{
@@ -10,6 +11,7 @@ public class Main_class extends Application{
     @Override
     public void onCreate() {
         super.onCreate();
+        FirebaseMessaging.getInstance().setAutoInitEnabled(true);
         new Prefs.Builder()
                 .setContext(this)
                 .setMode(ContextWrapper.MODE_PRIVATE)
